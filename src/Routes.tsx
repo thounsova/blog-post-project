ea2af0761d96dc1fb93ede8fab5514606cc5f15e
thounsova <thoun.sovannareach@institute.pse.ngo>
2025-05-08 08:27:10 +0700

    create Routes to page contact and blog page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
+import Contact from "./Pages/Contact/contact";
 import Home from "./Pages/Home/Home";
-import StoryGrid from "./Pages/productblog/productblog";
+import Productblog from "./Pages/productblog/productblog";
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
 
@@ -9,8 +10,10 @@ const AppRoute = () => {
     // Fixed return statement (was missing parentheses and had extra curly braces)
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/productblog" element={<StoryGrid />} />
+      <Route path="/productblog" element={<Productblog />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/contact" element={<Contact />} />
+
       <Route path="/register" element={<Register />} />
     </Routes>
   );
